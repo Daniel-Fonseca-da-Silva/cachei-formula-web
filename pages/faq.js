48901a17678f0eefb989f8a67aecf7459ba4189f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-03-01 14:15:42 -0300

    feat: Created the project with home and faq pages

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FaqPage() {
+  return (
+    <div>
+      <h1>Paginas de perguntas</h1>
+      <a href="/">I para a home</a>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Cachei Formula Web</h1>
+      <a href="/faq">I para o FAQ</a>
+    </div>
+  )
+}
